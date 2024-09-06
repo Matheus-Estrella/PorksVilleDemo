29c057bf5be3a0128568d0d46763a0e9dfea9e73
Joao Pedro Romao <ojpzinho@gmail.com>
2024-09-06 19:47:09 -0300

    debugger

--- open_game.bat
+++ open_game.bat
@@ -1,1 +1,8 @@
-python3 ./code/main.py
+set code=./code/main.py
+set user=%USERNAME%
+set python_path=python
+set debugpy_path=C:/Users/%user%/.vscode/extensions/ms-python.debugpy-2024.10.0-win32-x64/bundled/libs/debugpy/launcher
+set port=52801
+set host=127.0.0.1
+
+cmd /C "%python_path% -m debugpy --listen %host%:%port% --wait-for-client %code%"
